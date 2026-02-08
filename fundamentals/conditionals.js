559de5b90a8824d6081f8b2f3f32efb62a546d44
Zan-prog <pedrozancanaro47@gmail.com>
2026-02-08 13:26:05 -0300

    Create conditionals.js

---
+++ fundamentals/conditionals.js
@@ -0,0 +1,32 @@
+// File: conditionals.js
+// Learning conditional statements in JavaScript
+// Technical IT course - IFSul Passo Fundo
+
+let age = 16;
+
+// Simple if / else
+if (age >= 18) {
+  console.log("You are an adult.");
+} else {
+  console.log("You are under 18.");
+}
+
+// if / else if / else
+let grade = 7;
+
+if (grade >= 9) {
+  console.log("Excellent!");
+} else if (grade >= 6) {
+  console.log("Approved.");
+} else {
+  console.log("Failed.");
+}
+
+// Using boolean variables
+let isStudent = true;
+
+if (isStudent) {
+  console.log("The user is a student.");
+} else {
+  console.log("The user is not a student.");
+}
