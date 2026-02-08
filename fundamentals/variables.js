3b503adf98125494c1847c0c9256aad4ec0ad772
Zan-prog <pedrozancanaro47@gmail.com>
2026-02-08 13:14:53 -0300

    Update and rename variants.js to variables.js

---
+++ fundamentals/variables.js
@@ -0,0 +1,21 @@
+//File: variables.js
+// Learning JavaScript variables
+// Technical IT course - IFSul Passo Fundo
+
+//Variable declarations
+let name = "Student";
+let age = 16;
+let studyingIT = true;
+
+// Printing values to the console
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Studying IT?:", studyingIT);
+
+// Changing variable value
+age = 17
+console.log ("New age", age);
+
+// Constant example
+const school = "IFSul"
+console.log("Shool:", school
