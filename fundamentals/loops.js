6062fd2608b160c6c6ee0c2e4d31a5e3dfd5b23c
Zan-prog <pedrozancanaro47@gmail.com>
2026-02-08 13:29:01 -0300

    Create loops.js

---
+++ fundamentals/loops.js
@@ -0,0 +1,30 @@
+// File: loops.js
+// Learning loops in JavaScript
+// Technical IT course - IFSul Passo Fundo
+
+// for loop
+console.log("For loop example:");
+
+for (let i = 1; i <= 5; i++) {
+  console.log("Number:", i);
+}
+
+// while loop
+console.log("While loop example:");
+
+let count = 1;
+
+while (count <= 5) {
+  console.log("Count:", count);
+  count++;
+}
+
+// do...while loop
+console.log("Do...while loop example:");
+
+let x = 1;
+
+do {
+  console.log("Value:", x);
+  x++;
+} while (x <= 3);
