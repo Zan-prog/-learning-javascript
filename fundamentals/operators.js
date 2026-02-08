c7f1593fe6abd6dd87a9d47f3e59f1b3718f5ac3
Zan-prog <pedrozancanaro47@gmail.com>
2026-02-08 13:23:40 -0300

    Create operators.js

---
+++ fundamentals/operators.js
@@ -0,0 +1,26 @@
+// File: operator.js
+// Learning JavaScript operators
+// Technical IT course - IFSul Passo Fundo
+
+// Arithmetic operators
+let a = 10;
+let b = 5;
+
+console.log("Addition", a + b);
+console.log("Subtraction", a - b);
+console.log("Multiplication", a * b);
+console.log("Division", a / b);
+
+// Comparison operators
+console.log("a > b:", a > b);
+console.log("a < b:", a < b);
+console.log("a === b:", a === b);
+console.log("a !== b:", a !== b);
+
+// Logical operators
+let isStudent = true
+let hasTime = false
+
+console.log("isStudent AND hasTime:", isStudent && hasTime);
+console.log("isStudent OR hasTime:", isStudent || hasTime);
+console.log("NOT isStudent:", !isStudent);
